8ce078228ba455d6ce272ce5f7731be8b802e0b7
Josh Peck <jmp@joshpeck.org>
2011-01-24 10:30:34 -0600

    updating version and committing new rubyforge release params

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,57 +3,29 @@ load 'tasks/testtask.rake'
 load 'tasks/doctask.rake'
 
 require 'rubygems'
-require "rake/gempackagetask"
 require 'hoe'
+require 'rubyforge'
 
 # Hoe.plugin :compiler
 # Hoe.plugin :gem_prelude_sucks
 # Hoe.plugin :inline
 # Hoe.plugin :inline
 # Hoe.plugin :racc
-# Hoe.plugin :rubyforge
+Hoe.plugin :rubyforge
 # Hoe.plugin :rubyforge
 
-Hoe.spec 'adcenter_client' do
-  developer('Josh Peck', 'jmp@joshpeck.org')
-  name = "adcenter-client"
-  version = "7.0.0"
-  author = "Josh Peck"
-  email = "jmp@joshpeck.org"
-  homepage = "http://bleechack.github.com/adcenter-client/"
-  platform = Gem::Platform::RUBY
-  summary = "provides access to the Microsoft adCenter SOAP API"
-  files = FileList["{bin,lib}/**/*"].to_a
-  require_path = "lib"
-  autorequire = "soap4r"
-  test_files = FileList["{test}/**/*test.rb"].to_a
-  has_rdoc = true
-  extra_rdoc_files = [ "README.txt", 'History.txt' ]
-  rubyforge_project = 'adcenter-client'
-  # self.rubyforge_name = 'adcenter_clientx' # if different than 'adcenter_client'
+Hoe.spec 'adcenter-client' do |h|
+  h.summary = "SOAP client for MSN adCenter"
+  h.email = "jmp@joshpeck.org"
+  h.author = "Josh Peck"
+  h.url = "http://bleechack.github.com/adcenter-client/"
+  h.description = h.paragraphs_of('README.txt', 1..2).join("\n\n") 
+  h.changes = h.paragraphs_of('History.txt', 2..3).join("\n\n") 
+  h.remote_rdoc_dir = 'adcenter-client' 
+  h.developer('Josh Peck', 'jmp@joshpeck.org')
+  h.rubyforge_name = 'adcenter-client'
+  h.extra_deps << 'soap4r'
+  h.extra_deps << 'hoe'
 end
-
-# spec = Gem::Specification.new do |s| 
-#   s.name = "adcenter-client"
-#   s.version = "7.0.0"
-#   s.author = "Josh Peck"
-#   s.email = "jmp@joshpeck.org"
-#   s.homepage = "hhttps://github.com/bleechack/adcenter-client"
-#   s.platform = Gem::Platform::RUBY
-#   s.summary = "provides access to the Microsoft adCenter SOAP API"
-#   s.files = FileList["{bin,lib}/**/*"].to_a
-#   s.require_path = "lib"
-#   s.autorequire = "soap4r"
-#   s.test_files = FileList["{test}/**/*test.rb"].to_a
-#   s.has_rdoc = true
-#   s.extra_rdoc_files = [ "README.txt", 'History.txt' ]
-#   s.rubyforge_project = 'adcenter-client'
-#   # s.add_dependency("dependency", ">= 0.x.x")
-# end
-# 
-# Rake::GemPackageTask.new(spec) do |pkg| 
-#   pkg.need_tar = true 
-# end 
-
 
 # vim: syntax=ruby
